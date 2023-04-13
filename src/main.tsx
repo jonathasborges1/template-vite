6a55632e910fd04aae47b96316378d6f7e6b840c
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-04-13 01:02:54 -0400

    Genesis: SnackbarProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CssBaseline } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 
 import RouterProvider from '@components/routes';
 import ThemeProvider from '@theme/index.js';
@@ -11,8 +12,10 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <CssBaseline />
-      <RouterProvider/>
+      <SnackbarProvider>
+        <CssBaseline />
+        <RouterProvider/>
+      </SnackbarProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
